055f627292d54bbf8b67866cb4526fb53ce543a9
KhaledAbdellaty <kabdellaty@gmail.com>
2024-04-24 03:05:56 -0700

    3. Dictionary of list of dictionaries

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Script that list To-do information for a employee Id."""
+import csv
 import requests
 import sys
-import csv
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Script that list To-do information for a employee Id."""
+import json
 import requests
 import sys
-import json
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
