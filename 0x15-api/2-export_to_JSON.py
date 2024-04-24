a816b365960ed81287555878d33b30eb4d853c9f
KhaledAbdellaty <kabdellaty@gmail.com>
2024-04-24 03:00:05 -0700

    2. Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Script that list To-do information for a employee Id."""
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    with open("{}.json".format(sys.argv[1]), "w") as jsonfile:
+        json.dump({sys.argv[1]: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
